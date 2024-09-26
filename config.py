b62f6f7f0c20fdcb1717d2606af72b994e045e17
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-09-26 11:06:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7647801633:AAGI535NE_0N2IHdAt8zvGnMux9TVVq1i2o")
+API_ID = int(os.environ.get("API_ID", "25373607"))
+API_HASH = os.environ.get("API_HASH", "3b559c2461a210c9654399b66125bc0b")
 SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
