438f8a605cc331f097e7eea054486f7eaec293c4
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-01 12:41:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ moderators = {}
 # Your user ID and username
 OWNER_ID = 6849853752
 OWNER_USERNAME = "rifiutoatomico"
-CO_OWNER_ID = 5318754238
-CO_OWNER_USERNAME = "Aanubi"
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, update):
@@ -31,16 +29,20 @@ Thanks for using Telegram Group Guardian! Let's keep your group safe and respect
 
 @Bot.on_message(filters.group & filters.command("pex"))
 async def promote_helper(bot, message):
-    if message.from_user.id in [OWNER_ID, CO_OWNER_ID] or message.from_user.username in [OWNER_USERNAME, CO_OWNER_USERNAME]:
+    if message.from_user.id == OWNER_ID or message.from_user.username == OWNER_USERNAME:
         if len(message.command) > 1:
             username = message.command[1].lstrip('@')
             user = await bot.get_users(username)
-            helpers[user.id] = message.chat.id
-            await message.reply(f"{user.first_name} has been promoted to helper in this group!")
+            if "mod" in message.command:
+                moderators[user.id] = message.chat.id
+                await message.reply(f"{user.first_name} has been promoted to moderator in this group!")
+            else:
+                helpers[user.id] = message.chat.id
+                await message.reply(f"{user.first_name} has been promoted to helper in this group!")
         else:
             await message.reply("Please provide a username to promote.")
     else:
-        await message.reply("Only the bot owner and co-owner can promote helpers.")
+        await message.reply("Only the bot owner can promote helpers or moderators.")
 
 @Bot.on_message(filters.group & filters.command("cancella"))
 async def delete_message(bot, message):
@@ -115,20 +117,4 @@ async def kick_user(bot, message):
         elif message.reply_to_message:
             user_id = message.reply_to_message.from_user.id
             await bot.kick_chat_member(message.chat.id, user_id)
-            await message.reply(f"{message.reply_to_message.from_user.first_name} è stato espulso permanentemente da {message.from_user.first_name}!")
-        else:
-            await message.reply("Per favore fornisci un username, un ID o rispondi all'utente che vuoi espellere.")
-    else:
-        await message.reply("Non sei autorizzato a usare questo comando.")
-
-@Bot.on_message(filters.group & filters.command("riavvia"))
-async def restart_bot(bot, message):
-    if message.from_user.id in [OWNER_ID, CO_OWNER_ID] or message.from_user.username in [OWNER_USERNAME, CO_OWNER_USERNAME]:
-        await message.reply("Il bot si sta riavviando...")
-        await bot.stop()
-        await bot.start()
-        await message.reply("Il bot è stato riavviato con successo!")
-    else:
-        await message.reply("Non sei autorizzato a usare questo comando.")
-
-Bot.run()
+            await message.reply
