571834887c7e02987fd3db2f9b87f30040469561
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-03 16:34:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ async def mute_for_link(bot, message):
             permissions=ChatPermissions(
                 can_send_messages=True,
                 can_send_media_messages=True,
-                can send_polls=True,
+                can_send_polls=True,
                 can_send_other_messages=True,
                 can_add_web_page_previews=True,
                 can_change_info=False,
