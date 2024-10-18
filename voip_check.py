5e4f913abc2fb4bd60d3b67e5e82f131e0cc9a28
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-18 23:06:35 +0200

    Delete voip_check.py

--- voip_check.py
+++
@@ -1,9 +0,0 @@
-# voip_check.py
-
-voip_prefixes = ["+1", "+44", "+91", "+81", "+86", "+33", "+49", "+7", "+61", "+55", "+39"]
-
-def check_voip_prefix(phone_number):
-    for prefix in voip_prefixes:
-        if phone_number.startswith(prefix):
-            return True
-    return False
