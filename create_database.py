9febe3843280a39544eca2b77d2246890da28267
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-20 10:51:26 +0200

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -5,7 +5,10 @@ conn = sqlite3.connect('user_ips.db')
 c = conn.cursor()
 
 # Crea la tabella per gli IP
-c.execute('''CREATE TABLE IF NOT EXISTS ips (user_id INTEGER PRIMARY KEY, ip_address TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS ips (
+    user_id INTEGER PRIMARY KEY,
+    ip_address TEXT
+)''')
 
 # Salva le modifiche e chiudi la connessione
 conn.commit()
