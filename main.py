15b647c1b1a7288eecf7785fba93948ef6f91de5
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-23 19:32:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import asyncio
 
 # Configurazione del logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig.level=logging.INFO
 
 bot = Client(
     "group_guardian",
@@ -15,6 +15,8 @@ bot = Client(
     api_id=config.API_ID,
     api_hash=config.API_HASH
 )
+
+GROUP_ID = -1002202385937
 
 # Memoria per gli IP
 ip_memory = {}
@@ -57,7 +59,7 @@ async def welcome_and_mute(client, message):
         button = InlineKeyboardButton(text="✅ Verifica", url=verification_link)
         keyboard = InlineKeyboardMarkup([[button]])
         await message.reply_text(f"Benvenuto {new_member.first_name}! Per favore, completa la verifica cliccando il bottone qui sotto.", reply_markup=keyboard)
-        await asyncio.sleep(10800)  # Aspetta 3 ore
+        await asyncio.sleep(180)  # Aspetta 3 minuti
         if new_member.id not in ip_memory:
             await ban_user(client, message.chat.id, new_member.id, "Tempo di verifica scaduto.")
 
@@ -71,23 +73,23 @@ async def verifica_callback(client, message):
         logging.info("IP dell'utente: %s, Codice Paese: %s", ip_address, country_code)
         
         if country_code != "IT":
-            await ban_user(client, message.chat.id, user_id, "IP non italiano rilevato.")
+            await ban_user(client, GROUP_ID, user_id, "IP non italiano rilevato.")
             unban_button = InlineKeyboardButton(text="Sblocca", callback_data=f"unban_{user_id}")
-            await client.send_message(message.chat.id, "Richiesta di sblocco inviata.", reply_markup=InlineKeyboardMarkup([[unban_button]]))
+            await client.send_message(GROUP_ID, "Richiesta di sblocco inviata.", reply_markup=InlineKeyboardMarkup([[unban_button]]))
         else:
             duplicate_users = is_duplicate_ip(ip_address)
             if duplicate_users:
                 for duplicate_user_id in duplicate_users:
-                    await ban_user(client, message.chat.id, int(duplicate_user_id), "Account multiplo rilevato.")
-                await ban_user(client, message.chat.id, user_id, "Account multiplo rilevato.")
+                    await ban_user(client, GROUP_ID, int(duplicate_user_id), "Account multiplo rilevato.")
+                await ban_user(client, GROUP_ID, user_id, "Account multiplo rilevato.")
                 unban_button = InlineKeyboardButton(text="Sblocca", callback_data=f"unban_{user_id}")
-                await client.send_message(message.chat.id, "Richiesta di sblocco inviata.", reply_markup=InlineKeyboardMarkup([[unban_button]]))
+                await client.send_message(GROUP_ID, "Richiesta di sblocco inviata.", reply_markup=InlineKeyboardMarkup([[unban_button]]))
             else:
                 ip_memory[user_id] = ip_address
-                confirmation_message = await client.send_message(message.chat.id, f"Verifica completata con successo per {message.from_user.first_name} {message.from_user.last_name}.")
+                confirmation_message = await client.send_message(GROUP_ID, f"Verifica completata con successo per {message.from_user.first_name} {message.from_user.last_name}.")
                 await client.send_message(user_id, "Verifica completata con successo.")
                 await client.restrict_chat_member(
-                    message.chat.id,
+                    GROUP_ID,
                     user_id,
                     ChatPermissions(
                         can_send_messages=True,
@@ -96,32 +98,15 @@ async def verifica_callback(client, message):
                         can_add_web_page_previews=True
                     )
                 )
-                await asyncio.sleep(10800)  # Aspetta 3 ore
-                await client.delete_messages(message.chat.id, confirmation_message.message_id)
     else:
-        error_message = await client.send_message(message.chat.id, f"Errore nella verifica per {message.from_user.first_name} {message.from_user.last_name}. Riprova più tardi.")
+        error_message = await client.send_message(GROUP_ID, f"Errore nella verifica per {message.from_user.first_name} {message.from_user.last_name}. Riprova più tardi.")
         await client.send_message(user_id, "Errore nella verifica. Riprova più tardi.")
-        await asyncio.sleep(10800)  # Aspetta 3 ore
-        await client.delete_messages(message.chat.id, error_message.message_id)
 
 @bot.on_callback_query(filters.regex(r"unban_"))
 async def unban_callback(client, callback_query):
     user_id = int(callback_query.data.split("_")[1])
     chat_id = callback_query.message.chat.id
     await unban_user(client, chat_id, user_id)
-
-@bot.on_message(filters.command("start"))
-async def start(client, message):
-    welcome_text = (
-        "Ciao! Sono un bot anti-account multipli. "
-        "Aggiungimi al tuo gruppo per proteggere dai VoIP!\n\n"
-        "✅ Aggiungimi ad un gruppo\n"
-        "📜 [Privacy Policy](https://t.me/PrivacyAndPolicyCn)"
-    )
-    await message.reply_text(welcome_text, reply_markup=InlineKeyboardMarkup([
-        [InlineKeyboardButton("➕ Aggiungi ad un gruppo", url=f"https://t.me/{client.me.username}?startgroup=true")],
-        [InlineKeyboardButton("📜 Privacy Policy", url="https://t.me/PrivacyAndPolicyCn")]
-    ]))
 
 # Avvia il bot
 bot.run()
