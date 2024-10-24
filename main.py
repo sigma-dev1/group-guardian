cf0e4712b78f5638dfce1fcf29bdbaa5f47b4246
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-10-24 11:34:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ GROUP_IDS = [
 
 # Memoria per gli IP
 ip_memory = {}
-message_ids = {}
 
 # Funzione per ottenere l'IP e la geolocalizzazione
 async def get_ip_and_location():
@@ -53,15 +52,6 @@ async def unban_user(client, chat_id, user_id):
     await client.unban_chat_member(chat_id, user_id)
     await client.send_message(chat_id, f"{user_id} è stato sbloccato.")
 
-# Comando per cancellare i messaggi del bot
-@bot.on_message(filters.command("cancella"))
-async def cancella(client, message):
-    if message.chat.id in message_ids:
-        for msg_id in message_ids[message.chat.id]:
-            await client.delete_messages(message.chat.id, msg_id)
-        message_ids[message.chat.id] = []
-        await message.reply_text("Messaggi cancellati con successo.")
-
 @bot.on_message(filters.new_chat_members)
 async def welcome_and_mute(client, message):
     for new_member in message.new_chat_members:
@@ -75,11 +65,10 @@ async def welcome_and_mute(client, message):
         button = InlineKeyboardButton(text="✅ Verifica", url=verification_link)
         keyboard = InlineKeyboardMarkup([[button]])
         welcome_message = await message.reply_text(f"Benvenuto {new_member.first_name or new_member.username}! Per favore, completa la verifica cliccando il bottone qui sotto.", reply_markup=keyboard)
-        message_ids.setdefault(message.chat.id, []).append(welcome_message.message_id)
         await asyncio.sleep(180)  # Aspetta 3 minuti
         if new_member.id not in ip_memory:
             await ban_user(client, message.chat.id, new_member.id, f"{new_member.first_name or new_member.username} non ha passato la verifica ed è stato bannato.")
-            await client.delete_messages(message.chat.id, [welcome_message.message_id])
+            await client.delete_messages(message.chat.id, welcome_message.message_id)
 
 @bot.on_message(filters.regex(r"^/start verifica_\d+$"))
 async def verifica_callback(client, message):
@@ -91,20 +80,16 @@ async def verifica_callback(client, message):
         logging.info("IP dell'utente: %s, Codice Paese: %s", ip_address, country_code)
         
         if country_code != "IT":
-            ban_msg = await ban_user(client, message.chat.id, user_id, f"{message.from_user.first_name or message.from_user.username} non ha passato la verifica ed è stato bannato per essere un account multiplo.")
-            message_ids.setdefault(message.chat.id, []).append(ban_msg.message_id)
+            await ban_user(client, message.chat.id, user_id, f"{message.from_user.first_name or message.from_user.username} non ha passato la verifica ed è stato bannato per essere un account multiplo.")
         else:
             duplicate_users = is_duplicate_ip(ip_address)
             if duplicate_users:
                 for duplicate_user_id in duplicate_users:
-                    ban_msg_dup = await ban_user(client, message.chat.id, int(duplicate_user_id), "Account multiplo rilevato.")
-                    message_ids.setdefault(message.chat.id, []).append(ban_msg_dup.message_id)
-                ban_msg_multi = await ban_user(client, message.chat.id, user_id, f"{message.from_user.first_name or message.from_user.username} non ha passato la verifica ed è stato bannato per essere un account multiplo.")
-                message_ids.setdefault(message.chat.id, []).append(ban_msg_multi.message_id)
+                    await ban_user(client, message.chat.id, int(duplicate_user_id), "Account multiplo rilevato.")
+                await ban_user(client, message.chat.id, user_id, f"{message.from_user.first_name or message.from_user.username} non ha passato la verifica ed è stato bannato per essere un account multiplo.")
             else:
                 ip_memory[user_id] = ip_address
                 confirmation_message = await client.send_message(message.chat.id, f"Verifica completata con successo per {message.from_user.first_name or message.from_user.username}.")
-                message_ids.setdefault(message.chat.id, []).append(confirmation_message.message_id)
                 await client.send_message(user_id, "Verifica completata con successo.")
                 await client.restrict_chat_member(
                     message.chat.id,
